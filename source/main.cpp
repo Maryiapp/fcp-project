30933b39d0d18e577db30a6eea1f6b546523568b
Maryiapp <pilipchukmaria8@gmail.com>
2026-01-31 16:10:06 +0300

    Refactor main function and add usage manual
    Updated main function to handle command-line arguments and added usage instructions.

--- source/main.cpp
+++ source/main.cpp
@@ -1,40 +1,76 @@
+/**
+ * @file main.cpp
+ * @brief Entry point of the K-Means clustering program.
+ *
+ * This program performs K-Means clustering on a dataset
+ * provided in CSV format.
+ *
+ * @author Maryia Pilipchuk
+ * @date 2025
+ */
 
-#include <string>
 #include <iostream>
+#include <string>
+#include "kmeans.h"
 
-#include "functions.h"
-#include "structures.h"
+using namespace std;
 
-int main (int number_params, char * params[])
-{
-    std::pair<std::string,std::string> input_output_names = read_names_of_files (number_params, params);
-    // std::pair<std::string,std::string>  read_names_of_files (int number_params, char * params[]);
+/**
+ * @brief Prints program usage instructions.
+ */
+void printManual() {
+    cout << "Simple K-Means Program\n\n";
+    cout << "Usage:\n Project_1.exe -i Iris.csv -o out.txt -k 3\n\n";
+}
 
-    // std::cout << "input file name:  " << input_output_names.first << std::endl;
-    // std::cout << "output file name: " << input_output_names.second << std::endl;
+/**
+ * @brief Main function of the program.
+ *
+ * Parses command-line arguments and runs the K-Means algorithm.
+ *
+ * @param argc Number of command-line arguments
+ * @param argv Array of command-line arguments
+ * @return int Exit status code
+ */
+int main(int argc, char* argv[]) {
 
-    if (not input_output_names.first.empty() and not input_output_names.second.empty())
-    {
-        // OK
-        graph conflicts = read_conflicts(input_output_names.first);
-        // graph read_conflicts (const std::string & input_file_name);
-
-        print(conflicts);
-        // void print(const graph & g);
-
-        colour_nodes(conflicts);
-        // void colour_nodes(graph & g);
-        print(conflicts);
-
-
-        print_colours(conflicts, input_output_names.second);
-        //  void print_colours (const graph & g, const std::string & output_file_name);
+    if (argc == 1) {
+        printManual();
+        return 0;
     }
-    else
-    {
-      help(params);
-      // void help(char * params[]);
+
+    string inputFile, outputFile;
+    int k = 0;
+
+    for (int i = 1; i < argc; i++) {
+        string arg = argv[i];
+
+        if (arg == "-i" && i + 1 < argc) {
+            inputFile = argv[++i];
+        }
+        else if (arg == "-o" && i + 1 < argc) {
+            outputFile = argv[++i];
+        }
+        else if (arg == "-k" && i + 1 < argc) {
+            k = stoi(argv[++i]);
+        }
+        else {
+            cout << "Unknown argument: " << arg << "\n";
+            printManual();
+            return 1;
+        }
     }
+
+    if (inputFile.empty() || outputFile.empty() || k <= 0) {
+        printManual();
+        return 1;
+    }
+
+    KMeans model(k);
+    model.loadData(inputFile);
+    model.run();
+    model.saveResults(outputFile);
 
     return 0;
 }
+
