b4f6a653a94b00ecc8339540096c8c2646eba1be
Maryiapp <pilipchukmaria8@gmail.com>
2026-01-31 16:13:00 +0300

    Refactor functions.h to kmeans.h with KMeans class

--- source/functions.h
+++ source/functions.h
@@ -1,19 +1,87 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
+#ifndef KMEANS_H
+#define KMEANS_H
 
+/**
+ * @file kmeans.h
+ * @brief Declaration of KMeans class.
+ */
+
+#include <vector>
 #include <string>
-#include "structures.h"
+#include "point.h"
 
-void print_colours (const graph & g, const std::string & output_file_name);
+using namespace std;
 
-void colour_nodes(graph & g);
+/**
+ * @struct Centroid
+ * @brief Represents a centroid in K-Means clustering.
+ */
+struct Centroid {
+    /// Coordinates of centroid
+    vector<double> coords;
+};
 
-void print(const graph & g);
+/**
+ * @class KMeans
+ * @brief Implements the K-Means clustering algorithm.
+ *
+ * This class loads data, performs clustering,
+ * and saves the results to a file.
+ */
+class KMeans {
+public:
 
-graph read_conflicts (const std::string & input_file_name);
+    /**
+     * @brief Constructor.
+     * @param k Number of clusters.
+     */
+    KMeans(int k);
 
-void help(char * params[]);
+    /**
+     * @brief Loads dataset from CSV file.
+     * @param filename Path to input file.
+     */
+    void loadData(const string& filename);
 
-std::pair<std::string,std::string>  read_names_of_files (int number_params, char * params[]);
+    /**
+     * @brief Runs the clustering algorithm.
+     */
+    void run();
+
+    /**
+     * @brief Saves clustering results to file.
+     * @param filename Path to output file.
+     */
+    void saveResults(const string& filename);
+
+private:
+    int k; ///< Number of clusters
+    int dimensions = 0; ///< Number of features (dimensions)
+
+    vector<Point> points; ///< Dataset points
+    vector<Centroid> centroids; ///< Cluster centroids
+
+    /// Initializes centroids randomly
+    void initCentroids();
+
+    /// Assigns each point to nearest centroid
+    void assignClusters();
+
+    /// Updates centroid positions
+    void updateCentroids();
+
+    /**
+     * @brief Computes squared Euclidean distance.
+     */
+    double distance(const Point& p, const Centroid& c);
+
+    /**
+     * @brief Checks convergence condition.
+     * @param oldCentroids Previous centroid positions
+     * @param tol Tolerance threshold
+     * @return true if converged
+     */
+    bool hasConverged(const vector<Centroid>& oldCentroids, double tol);
+};
 
 #endif
